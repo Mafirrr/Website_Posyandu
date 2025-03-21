9c07ebfe310d07484a5f369e96d61c278d45e9c7
newbieflank <raninovia188@gmail.com>
2025-03-21 21:12:06 +0700

    update migration Skrining

--- app/Models/Skrining.php
+++ app/Models/Skrining.php
@@ -2,9 +2,106 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Skrining extends Model
 {
-    //
+    use HasFactory, SoftDeletes;
+
+    protected $table = 'skrining';
+
+    protected $fillable = [
+        'kehamilan_id',
+        'multipara_pasangan_baru',
+        'kehamilan_teknologi',
+        'umur_35_keatas',
+        'multipara_jarak_10_tahun_keatas',
+        'riwayat_preeklampsia',
+        'multipara_riwayat_preeklampsia',
+        'multipel_kehamilan',
+        'diabetes',
+        'hipertensi_kronis',
+        'penyakit_ginjal',
+        'autonium_sle',
+        'anti_phospholipid_syndrome',
+        'pemeriksaan_fisik',
+        'mean_arterial_pressure',
+        'proteinuria',
+        'obesitas_sebelum_hamil',
+        'status_risiko',
+    ];
+
+    protected $casts = [
+        'multipara_pasangan_baru' => 'boolean',
+        'kehamilan_teknologi' => 'boolean',
+        'umur_35_keatas' => 'boolean',
+        'multipara_jarak_10_tahun_keatas' => 'boolean',
+        'riwayat_preeklampsia' => 'boolean',
+        'multipara_riwayat_preeklampsia' => 'boolean',
+        'multipel_kehamilan' => 'boolean',
+        'diabetes' => 'boolean',
+        'hipertensi_kronis' => 'boolean',
+        'penyakit_ginjal' => 'boolean',
+        'autonium_sle' => 'boolean',
+        'anti_phospholipid_syndrome' => 'boolean',
+        'pemeriksaan_fisik' => 'boolean',
+        'mean_arterial_pressure' => 'boolean',
+        'proteinuria' => 'boolean',
+        'obesitas_sebelum_hamil' => 'boolean',
+    ];
+
+
+    private $risiko_sedang = [
+        'multipara_pasangan_baru',
+        'kehamilan_teknologi',
+        'umur_35_keatas',
+        'multipara_jarak_10_tahun_keatas',
+        'riwayat_preeklampsia',
+        'obesitas_sebelum_hamil'
+    ];
+
+    private $risiko_tinggi = [
+        'multipara_riwayat_preeklampsia',
+        'multipel_kehamilan',
+        'diabetes',
+        'hipertensi_kronis',
+        'penyakit_ginjal',
+        'autonium_sle',
+        'anti_phospholipid_syndrome',
+        'mean_arterial_pressure',
+        'proteinuria',
+    ];
+
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::saving(function ($skrining) {
+            $skrining->status_risiko = $skrining->tentukanRisiko();
+        });
+    }
+
+
+    public function tentukanRisiko()
+    {
+        $jumlah_sedang = collect($this->risiko_sedang)->sum(fn($item) => $this->$item ? 1 : 0);
+        $jumlah_tinggi = collect($this->risiko_tinggi)->sum(fn($item) => $this->$item ? 1 : 0);
+
+        if ($jumlah_tinggi >= 1) {
+            return 'tinggi';
+        } elseif ($jumlah_sedang >= 2) {
+            return 'sedang';
+        } else {
+            return 'rendah';
+        }
+    }
+
+
+    public function kehamilan()
+    {
+        return $this->belongsTo(Kehamilan::class);
+    }
 }
--- database/migrations/2025_03_19_144437_create_skrinings_table.php
+++ database/migrations/2025_03_19_144437_create_skrinings_table.php
@@ -13,7 +13,25 @@ return new class extends Migration
     {
         Schema::create('skrining', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('kehamilan_id')->references('id')->on('kehamilan');
+            $table->foreignId('kehamilan_id')->constrained('kehamilan');
+            $table->boolean('multipara_pasangan_baru')->default(false);
+            $table->boolean('kehamilan_teknologi')->default(false);
+            $table->boolean('umur_35_keatas')->default(false);
+            $table->boolean('multipara_jarak_10_tahun_keatas')->default(false);
+            $table->boolean('riwayat_preeklampsia')->default(false);
+            $table->boolean('multipara_riwayat_preeklampsia')->default(false);
+            $table->boolean('multipel_kehamilan')->default(false);
+            $table->boolean('diabetes')->default(false);
+            $table->boolean('hipertensi_kronis')->default(false);
+            $table->boolean('penyakit_ginjal')->default(false);
+            $table->boolean('autonium_sle')->default(false);
+            $table->boolean('anti_phospholipid_syndrome')->default(false);
+            $table->boolean('pemeriksaan_fisik')->default(false);
+            $table->boolean('mean_arterial_pressure')->default(false);
+            $table->boolean('proteinuria')->default(false);
+            $table->boolean('obesitas_sebelum_hamil')->default(false);
+            $table->enum('status_risiko', ['rendah', 'sedang', 'tinggi'])->default('rendah');
+            $table->softDeletes();
             $table->timestamps();
         });
     }
