8a2205dbe1604e1dd694a1c9c5f606d685df3308
Mafirrr <firaaurelsf@gmail.com>
2025-05-21 20:29:04 +0700

    petugas

--- database/seeders/PetugasSeeder.php
+++ database/seeders/PetugasSeeder.php
@@ -12,6 +12,6 @@ class PetugasSeeder extends Seeder
      */
     public function run(): void
     {
-        Petugas::factory()->count(5)->create();
+        Petugas::factory()->count(1)->create();
     }
 }
